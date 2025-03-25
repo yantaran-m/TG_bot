99e2854911108b5ca19274b1802730eb9d047a32
yantaran <zieklass2@gmail.com>
2025-03-25 23:47:00 +0200

    Первый коммит

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
